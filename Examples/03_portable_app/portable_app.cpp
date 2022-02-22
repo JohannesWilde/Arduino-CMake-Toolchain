fbed0f1cf2ea2bd1fb99db783e27b7e335b44c6c
Johannes Wilde <git@jowil.de>
2022-02-22 01:21:35 +0100

    Initialize non-static POD arrays.

--- Examples/03_portable_app/portable_app.cpp
+++ Examples/03_portable_app/portable_app.cpp
@@ -6,6 +6,8 @@
 // input/output (app_printf/app_scanf)
 #include <app_stdio.h>
 
+#include <string.h>
+
 void app_setup()
 {
 	// Initialize standard input/output
@@ -16,8 +18,9 @@ void app_loop()
 {
 	// Read some text and then print it
 	char buf[128];
-	app_printf("Enter text: ");
+    memset(buf, 0, sizeof(buf) * sizeof(buf[0]));
+    app_printf("Enter text: ");
 	app_scanf("%s", buf); // Warning: Overflow possibility
-	app_printf("Entered => %s\n", buf);
+    app_printf("\r\nEntered => %s\r\n", buf);
 }
 
